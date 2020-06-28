b928dd944bed0bba92dbb2ec9ea8c5a18f38eb4e
Andy <ndzzngzz@gmail.com>
2020-06-28 22:10:06 +0400

    added Navigation bar

--- App.js
+++ App.js
@@ -1,37 +1,25 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
-
-const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 /////1st Screen
 function ScreenOne({ navigation }) {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          if (route.name === 'Screen 1') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Screen 2') {
-            iconName = focused
-              ? 'ios-list-box'
-              : 'ios-list';
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
+
       tabBarOptions={{
         activeTintColor: 'darkblue',
         inactiveTintColor: 'gray',
-      }}
+        style: {
+          backgroundColor: 'white'
+
+        }      }}
     >
       <Tab.Screen name="Tab 1" component={TabOne} />
       <Tab.Screen name="Tab 2" component={TabTwo} />
@@ -41,10 +29,10 @@ function ScreenOne({ navigation }) {
 function TabOne() {
   return (
     <View>
-      <Text style={{ textAlign: 'center', marginTop: 300, fontWeight: 'bold' }}>
+      <Text style={{ textAlign: 'center', marginTop: 300, fontWeight: 'bold'}}>
         This is the 1st screen,
       </Text>
-      <Text style={{ textAlign: 'center', fontWeight: '300', color: 'grey' }}>
+      <Text style={{ textAlign: 'center', fontWeight: '300', color: 'grey'}}>
         viewing the 1st tab
       </Text>
     </View>
@@ -71,11 +59,19 @@ function ScreenTwo({ navigation }) {
   );
 }
 
+const MyTheme = {
+  colors: {
 
+    primary: 'rgb(0, 0, 0)',
+    background: 'rgb(242, 242, 242)',
+    //text: 'rgb(0, 0, 0)',
+    //border: 'rgb(199, 199, 204)',
+  },
+};
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <Drawer.Navigator initialRouteName="Screen 1">
         <Drawer.Screen name="Screen 1" component={ScreenOne} />
         <Drawer.Screen name="Screen 2" component={ScreenTwo} />
