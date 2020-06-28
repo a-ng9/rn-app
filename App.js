6cc36f712263e5e659345fe894281fb5dc523ccf
Andy <ndzzngzz@gmail.com>
2020-06-28 23:49:46 +0400

    added a theme and localisation

--- App.js
+++ App.js
@@ -5,19 +5,55 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
+//import { Localization} from 'expo-localization';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js'
+
+
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
+
+const en = {
+  feu: 'The fire',
+  bar: 'Bar {{someValue}}',
+};
+
+const fr = {
+  feu: 'Le feu',
+  bar: 'Bár {{someValue}}',
+};
+
+i18n.fallbacks = true;
+i18n.translations = { fr, en };
+
+ i18n.locale = Localization.locale;
+ i18n.locale = 'fr';
+
+
 
 /////1st Screen
 function ScreenOne({ navigation }) {
   return (
-    <Tab.Navigator
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
+          if (route.name === 'Tab 1') {
+            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+          } else if (route.name === 'Tab 2') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
       tabBarOptions={{
         activeTintColor: 'darkblue',
         inactiveTintColor: 'gray',
         style: {
-          backgroundColor: 'white'
+           backgroundColor: 'white'
 
         }      }}
     >
@@ -30,10 +66,13 @@ function TabOne() {
   return (
     <View>
       <Text style={{ textAlign: 'center', marginTop: 300, fontWeight: 'bold'}}>
-        This is the 1st screen,
+        This is the 1st screen
       </Text>
       <Text style={{ textAlign: 'center', fontWeight: '300', color: 'grey'}}>
         viewing the 1st tab
+      </Text>
+      <Text style={{ textAlign: 'center', fontWeight: '300', color: 'grey'}}>
+        Translated: {i18n.t('feu')}
       </Text>
     </View>
   );
@@ -49,6 +88,8 @@ function TabTwo() {
 }
 
 
+
+
 /////2nd Screen
 function ScreenTwo({ navigation }) {
   return (
@@ -61,9 +102,8 @@ function ScreenTwo({ navigation }) {
 
 const MyTheme = {
   colors: {
-
     primary: 'rgb(0, 0, 0)',
-    background: 'rgb(242, 242, 242)',
+    //background: 'rgb(242, 242, 242)',
     //text: 'rgb(0, 0, 0)',
     //border: 'rgb(199, 199, 204)',
   },
